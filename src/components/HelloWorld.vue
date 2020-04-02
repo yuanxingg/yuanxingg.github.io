c28c3b073059581eef7f639c5f05066b1b981c24
yuanxingg <915653097@qq.com>
2020-04-02 10:10:54 +0800

    项目替换

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <h2>？？？？</h2>
+    <ul>
+      <li>
+        <a
+          href="https://fuwu.jtyedu.com"
+          target="_blank"
+        >
+          jtyjxpt
+        </a>
+      </li>
+      <li>
+        <a
+          href="https://yuanxingg.github.io/"
+          target="_blank"
+        >
+          myPersonal
+        </a>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to YuanXing Web'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
